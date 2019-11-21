ac741154c873d5929dc6cbe8e27124380d06c055
Jack Pertschuk <jp954@cornell.edu>
2019-11-21 18:19:30 -0500

    added general distillation code

--- run_pretraining.py
+++ run_pretraining.py
@@ -64,7 +64,7 @@ flags.DEFINE_string(
   "Initial checkpoint (usually from a pre-trained BERT model).")
 
 flags.DEFINE_integer(
-  "max_seq_length", 128,
+  "max_seq_length", 512,
   "The maximum total input sequence length after WordPiece tokenization. "
   "Sequences longer than this will be truncated, and sequences shorter "
   "than this will be padded. Must match data generation.")
